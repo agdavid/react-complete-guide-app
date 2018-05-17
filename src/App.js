5ae4948579eefd64901f68ade786d9e8f3254763
Ajay David <antonio.g.david@gmail.com>
2018-05-16 20:29:15 -0400

    use onClick handler to update state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max' },
+      { name: 'Manu' },
+      { name: 'Stephanie' }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Maximilian' },
+        { name: 'Manute' },
+        { name: 'Steffi' }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to This Sample React App</h1>
-        <Person name="Max"/>
-        <Person name="Manu">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name}/>
+        <Person name={this.state.persons[1].name}>
           My hobbies include coding to change the world!
         </Person>
-        <Person name="Stephanie"/>
+        <Person name={this.state.persons[2].name}/>
       </div>
     );
   }
